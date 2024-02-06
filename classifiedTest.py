0f57d421109e18c54e7ecf47c199036ca7c4ce98
hs242424 <79582976+harrison-servedio@users.noreply.github.com>
2024-02-06 09:19:26 -0500

    Update classifiedTest.py
    for drawing order

--- classifiedTest.py
+++ classifiedTest.py
@@ -2,8 +2,8 @@ from classifiedOrbitCalculator import planet, plot, live
 
 
 ps = [
-    planet("Earth", 5.97219e24, 1.495978707e11, 0, 0, 29784, "Green"),
     planet("Moon", 7.3476e22, 3.84317695e8+1.495978707e11, 0, 0, 29784+1023, "Grey"),
+    planet("Earth", 5.97219e24, 1.495978707e11, 0, 0, 29784, "Green"),
     planet("Venus", 6.39e23, 114e9, 0, 0, 34500, "Pink"),
     planet("Mercury", 3.285e23, 69.814e9, 0, 0, 47000, "Silver"),
     planet("Sun", 1.989E+30, 0, 0, 0, 0, "Orange"),
